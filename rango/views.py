f1602a543ed0595851b4d56291a6e7b84b3f3c10
Jamie <j.g1992@live.co.uk>
2018-01-22 21:35:09 +0000

    Completed to the end of chapter 4. Pictures will not load.

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+    #Dictionary to pass the template engine as it's context
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    #Return a rendered response making use of the shortcut function
+    #First parameter is the template
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,7 +14,12 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# Builds paths to templates
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+# Builds paths to static
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+#Build paths to Media
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -50,12 +55,16 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+#Media links
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
 ROOT_URLCONF = 'tango_with_django_project.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -117,5 +127,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATICFILES_DIR =[STATIC_DIR, ]
 
 STATIC_URL = '/static/'
